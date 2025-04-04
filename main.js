13ed75bb4455bccee6951a488ad7bb32301e86ca
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2025-04-04 15:23:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,32 @@
 console.log('hi main.js')
-try{
-    let url = window.location.href
 
-    if (url.match('connect.router.integration.prod.mheducation.com/app/#/connect/coversheet')){
-        var begin_button = document.getElementsByClassName("btn btn-primary connect-primary-btn coversheet-btn-size")
-        if (begin_button){
-            begin_button[0].addEventListener('click', function(e) {
-                e.preventDefault();
-                const targetUrl = this.getAttribute('data-target-url');
-                console.log('New URL would be: ' + targetUrl);
-                var start = document.querySelector('[data-automation-id="welcome--start_button"]')
+document.addEventListener('DOMContentLoaded', function() {
+    try{
+        let url = window.location.href
 
-                if (start){
-                    start.click()
-                    console.log("start questions button clicked")
-                }
-                else{
-                    throw('start questions button not found by automation id')
-                }
-              });
-
-            begin_button[0].click()
+        if (url.includes('connect.router.integration.prod.mheducation.com/app/#/connect/coversheet')){
+            var begin_button = document.getElementsByClassName("btn btn-primary connect-primary-btn coversheet-btn-size")
+            if (begin_button){
+                begin_button[0].click()
+            }
+            else{
+                throw('begin button not found')
+            }
         }
-        else{
-            throw('begin button not found')
+        else if (url.includes('/learning.mheducation.com')) {
+            console.log('On learning page, waiting for start button...');
+            let checkStartButton = setInterval(() => {
+            let startButton = document.querySelector('[data-automation-id="welcome--start_button"]');
+            if (startButton) {
+                console.log('Start button found, clicking it...');
+                startButton.click();
+                clearInterval(checkStartButton);
+            }
+            }, 500);
         }
-    }
 
-    
-    url = window.location.href
-    if (url.match('/learning.mheducation.com')){
-        console.log('about to start questions')
     }
-    else{
-        throw("begin button led you to somewhere else")
+    catch(err){
+        console.log(err)
     }
-
-}
-catch{
-    console.log('some problem with fetching url')
-}
-
-try{
-    var start = document.querySelector('[data-automation-id="welcome--start_button"]')
-
-    if (start){
-        start.click()
-        console.log("start questions button clicked")
-    }
-    else{
-        throw('start questions button not found by automation id')
-    }
-
-}
-catch(err){
-    console.log("start questions button failed to click")
-    console.log(err)
-}
+})
