8445a37a54ec8be08f82781c4f808fe6e560102a
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2025-04-04 01:25:40 -0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-javascript:fetch("https://github.com/ma9c0/mcgraw-hill-hack/main.js").then(a=>a.text()).then(eval)
+javascript:fetch("https://raw.githubusercontent.com/ma9c0/mcgraw-hill-hack/main/main.js")
--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 try{
     let url = window.location.href
-    console.log(url)
+    if (url == 'https://connect.router.integration.prod.mheducation.com/app/#/connect/coversheet'){
+        console.log('url loaded successfully')
+    }
+    else{
+        console.log('wrong url')
+    }
 }
 catch{
-    console.log('some problem')
+    console.log('some problem with fetching url')
 }
+
