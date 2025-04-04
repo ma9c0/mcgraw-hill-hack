6c2899aa79700761e66f6ad4fd50288519ca0b4b
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2025-04-04 16:24:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,29 @@ console.log('hi main.js')
     if (url.includes('connect.router.integration.prod.mheducation.com/app/#/connect/coversheet')){
         const beginButton = document.querySelector('.btn.btn-primary.connect-primary-btn.coversheet-btn-size');
         if (beginButton){
-            let targetUrl = "";
-            (function(history) {
-                const originalPushState = history.pushState;
-                history.pushState = function(state, title, newurl) {
-                console.log('Navigating to:', newurl);
-                targetUrl = newurl;
-                return originalPushState.apply(history, arguments);
-                };
-            })(window.history);
+            beginButton.addEventListener('click', function(e){
+                e.preventDefault();
+                let targetUrl = "";
+                (function(history) {
+                    const originalPushState = history.pushState;
+                    history.pushState = function(state, title, newurl) {
+                    console.log('Navigating to:', newurl);
+                    targetUrl = newurl;
+                    return originalPushState.apply(history, arguments);
+                    };
+                })(window.history);
+
+                console.log("Intercepted begin button click. Target URL:", targetUrl);
+            })
+            // let targetUrl = "";
+            // (function(history) {
+            //     const originalPushState = history.pushState;
+            //     history.pushState = function(state, title, newurl) {
+            //     console.log('Navigating to:', newurl);
+            //     targetUrl = newurl;
+            //     return originalPushState.apply(history, arguments);
+            //     };
+            // })(window.history);
             
             beginButton.click();
 
@@ -24,8 +38,8 @@ console.log('hi main.js')
                 // Create an iframe to load the learning page
                 const iframe = document.createElement('iframe');
                 iframe.src = targetUrl;
-                iframe.style.width = '100%';
-                iframe.style.height = '600px'; // adjust height as needed
+                iframe.style.width = '50%';
+                iframe.style.height = '300px'; 
                 document.body.appendChild(iframe);
         
                 // When the iframe has loaded, search for the start button and click it.
