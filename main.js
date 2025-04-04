7f4ae7a43d7a4c1ff8f102633a6466e0d4d9c819
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2025-04-04 12:08:51 -0400

    test

--- main.js
+++ main.js
@@ -12,3 +12,6 @@ catch{
     console.log('some problem with fetching url')
 }
 
+document.body.insertAdjacentHTML("beforeend", '<div id="bennettc" style="z-index:1;position:absolute;width:400px;height:200px;background:#e6fff0;bottom:0;left:0;font-size:12px;font-family:monospace;"><input id="bennettin" style="width:400px;height:30px;" autocomplete="off"><div id="bennettl" style="display:flex;flex-direction:column-reverse;color:black;overflow-y:scroll;height:170px;"></div></div>')
+
+
