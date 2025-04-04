f91a0f7cbd0b86ae25eafbbded9bcfcebe4f7eb7
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2025-04-04 16:12:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,34 @@ console.log('hi main.js')
         if (beginButton){
             (function(history) {
                 const originalPushState = history.pushState;
-                history.pushState = function(state, title, url) {
-                console.log('Navigating to:', url);
-                window.open(url)
+                history.pushState = function(state, title, newurl) {
+                console.log('Navigating to:', newurl);
                 return originalPushState.apply(history, arguments);
                 };
             })(window.history);
-            beginButton.click()
+            
+            const iframe = document.createElement('iframe')
+            iframe.src = newurl
+            document.body.appendChild(iframe);
+            iframe.addEventListener('load', function() {
+                console.log('Learning page loaded in iframe.');
+                // Check periodically for the start button in the iframe's content.
+                const checkInterval = setInterval(function() {
+                  try {
+                    const startButton = iframe.contentWindow.document.querySelector('[data-automation-id="welcome--start_button"]');
+                    if (startButton) {
+                      console.log('Start button found inside iframe. Clicking it...');
+                      startButton.click();
+                      clearInterval(checkInterval);
+                    }
+                  } catch (e) {
+                    console.error('Error accessing iframe content:', e);
+                    clearInterval(checkInterval);
+                  }
+                }, 500);
+              });
+
+
         }
         else{
             alert('begin button not found')
